439d976aa9b9229f5490f7a703c3e025643b711e
jovanravael21 <jovanravael21@gmail.com>
2025-07-29 14:31:32 +1000

    I add the If conditional statement to initialize the description

--- Python/currency_regex.py
+++ Python/currency_regex.py
@@ -1,3 +1,7 @@
 import re
 pattern = r'^\$[0-9]{1,4}\.[0]{2}'
-print(re.match(pattern,"$.00"))
+
+if re.match(pattern,"$.00"):
+    print("The result mathces")
+else:
+    print("The result doesn't match")
