7d7fe753ebeaa1d55c643a09efe6a3f1e03727ca
jovanravael21 <jovanravael21@gmail.com>
2025-07-29 14:34:44 +1000

    Additional Changes

--- Python/currency_regex.py
+++ Python/currency_regex.py
@@ -2,6 +2,6 @@ import re
 pattern = r'^\$[0-9]{1,4}\.[0]{2}'
 
 if re.match(pattern,"$.00"):
-    print("The result mathces")
+    print("The result mathces with the description")
 else:
-    print("The result doesn't match")
+    print("The result doesn't match with the description")
