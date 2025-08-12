146b89af1c424833346810016286d498b0388d9e
jovanravael21 <jovanravael21@gmail.com>
2025-08-12 15:37:59 +1000

    new commit

--- Python/SpellingRule.py
+++ Python/SpellingRule.py
@@ -1,12 +1,14 @@
 while True:
     word = input("Enter a word: \n")
-    if word.lower() == "quiet":
+    if word.lower() == "quit":
         print("End program")
         break
-    if "ei" in word:
-       if "cei" not in word:
-           print(f"Potential spelling error found in: {word}")
-           print("Enter a word: ")
-           
+
+    if "ie" in word: # * Problem is needs to be "ie" not "ei"
+        if "cie" not in word:
+           print(f"Potential spelling error found in: {word}")  
+           # * I deleted the input for enter a word
+           # * You only need to use one input only because if it's not a spelling mistake
+           # * It will loop again until 
            break
  
